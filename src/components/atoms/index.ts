1171f079996d2fcbf791e270bc72afbb5dad2636
Alex Prapugicu <prapugicualex@gmail.com>
2024-07-16 07:48:06 +0300

    export input as well

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,5 @@
 export { default as Modal } from "./modal/modal";
 export { default as Button } from "./button/button";
 export { default as Separator } from "./separator/separator";
+export { default as Input } from "./input/input";
 export { default as Test } from "./separator/separator";
