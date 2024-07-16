23ddde800bf3685d5f52c74f2a86219cc245397a
Alex Prapugicu <prapugicualex@gmail.com>
2024-07-16 12:28:42 +0300

    export typography

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Modal } from "./modal/modal";
 export { default as Button } from "./button/button";
 export { default as Separator } from "./separator/separator";
 export { default as Input } from "./input/input";
+export { default as Typography } from "./typography/typography";
