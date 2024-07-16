22cfa06858381c4a985591e4c900297f2c41c249
Alex Prapugicu <prapugicualex@gmail.com>
2024-07-16 07:44:48 +0300

    rename back

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Modal } from "./modal/modal";
 export { default as Button } from "./button/button";
-export { default as Separator } from "./separator/test";
+export { default as Separator } from "./separator/separator";
+export { default as Test } from "./separator/separator";
