a4b433f15633790b4bab348d6d7642c789a0255d
Alex Prapugicu <prapugicualex@gmail.com>
2024-08-11 09:05:25 +0300

    export file select

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -7,6 +7,7 @@ export { default as Avatar } from "./avatar/avatar";
 export { default as Progress } from "./progress/progress";
 // Form components
 export { default as Input } from "./input/input";
+export { default as FileSelect } from "./fileSelect/fileSelect";
 export { default as Textarea } from "./textarea/textarea";
 export { default as Checkbox } from "./checkbox/checkbox";
 export { default as Radio } from "./radio/radio";
